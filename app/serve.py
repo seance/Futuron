bb953f6fee6035ff2f0b48d98bc8142b017da80f
Jukka Viinamäki <jukka.viinamaki@futurice.com>
2013-10-14 14:03:18 +0300

    Added CORS header serving Python web server script

---
+++ app/serve.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import SimpleHTTPServer
+
+class MyHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Access-Control-Allow-Origin", "*")
+        SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+
+if __name__ == '__main__':
+    SimpleHTTPServer.test(HandlerClass=MyHTTPRequestHandler)
